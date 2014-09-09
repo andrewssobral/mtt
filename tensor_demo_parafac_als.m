3aa7ffd5a74ac613c8a507be10208225d5e2839f
Andrews Sobral <andrewssobral@gmail.com>
2014-09-09 13:09:09 +0200

    Added parafac2 decomposition

--- tensor_demo_parafac_als.m
+++ tensor_demo_parafac_als.m
@@ -5,13 +5,14 @@ close all; clear all; clc;
 addpath('libs/poblano_toolbox_1.1');
 addpath('libs/tensor_toolbox_2.5');
 addpath('libs/nway331');
+addpath('libs/parafac2');
 
 %% LOAD DATASET
 load('dataset/trafficdb/traffic_patches.mat');
 A = double(imgdb{100});
 T = tensor(A);
 
-%% CP DECOMPOSITION
+%% PARAFAC/CP DECOMPOSITION via Alternating Least-Squares (ALS)
 n = size(A);
 r = 10;
 
@@ -22,6 +23,15 @@ r = 10;
 %   cp_opt         - Fits a CP model to a tensor via optimization.
 %   cp_wopt        - Fits a weighted CP model to a tensor via optimization.
 X = cp_als(T, r);
+
+%% PARAFAC2 DECOMPOSITION
+r = 10;
+[B,H,C,P,fit] = parafac2(A,r);
+
+%% PARAFAC2 reconstruction
+for i = 1:size(C,1)
+  X(:,:,i) = B*diag(C(i,:))*(P{i}*H)';
+end
 
 %% ERROR
 A_hat = double(X);
